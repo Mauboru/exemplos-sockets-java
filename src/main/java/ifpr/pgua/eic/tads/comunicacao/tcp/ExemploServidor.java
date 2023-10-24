2f0ac53c41b6571bbf34a7ab506ba0f8543a304b
Josué Henrique <121066937+Mauboru@users.noreply.github.com>
2023-10-24 13:16:21 -0300

    att

--- src/main/java/ifpr/pgua/eic/tads/comunicacao/tcp/ExemploServidor.java
+++ src/main/java/ifpr/pgua/eic/tads/comunicacao/tcp/ExemploServidor.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class ExemploServidor {
     public static void main(String[] args) {
         try{
-            ServidorTCP servidorTCP = new ServidorTCP("localhost",12000);
+            ServidorTCP servidorTCP = new ServidorTCP("10.20.3.148",12000);
             while (true){
                 servidorTCP.escuta();
                 servidorTCP.processa();
--- src/main/java/ifpr/pgua/eic/tads/comunicacao/tcp/Worker.java
+++ src/main/java/ifpr/pgua/eic/tads/comunicacao/tcp/Worker.java
@@ -1,7 +1,6 @@
 package ifpr.pgua.eic.tads.comunicacao.tcp;
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Worker implements Runnable{
